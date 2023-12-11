2e496e31e27c19b96ef664cf40ef22565f90987d
GarrettHaines <GarrettMHaines@gmail.com>
2023-12-10 18:32:20 -0800

    Removed old file

--- src/types/ware.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from "mongoose"
-
-export interface WaresInterface extends Document {
-  name: string
-  description: string
-  image: string
-  price: number
-  stock: number
-}
